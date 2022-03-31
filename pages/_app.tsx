63d852f6dfca5b46cca6d8a7b9e804e9579d1a7a
0xNeon-opensource <90662438+0xNeon-opensource@users.noreply.github.com>
2022-03-30 20:58:53 -0500

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import * as gtag from '../utils/gtag'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`} strategy='afterInteractive' />
+      <Script id="google-analytics" strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>)
 }
 
-export default MyApp
+export default App
